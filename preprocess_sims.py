c83f96bcb6e4e922057ab7d7993694f4a3228b81
Nathan Miller <nathan.j.miller@nasa.gov>
2024-06-14 13:14:15 -0400

    Current version

--- preprocess_sims.py
+++ preprocess_sims.py
@@ -45,7 +45,7 @@ def load_data(fn, event_hits, maxevents, minhits=1, detectortype=None, maxclass=
     if Reader.Open(M.MString(fn)) == False:
         print("Unable to open file " + fn + ". Aborting!")
         quit()
-
+    
     #Hist = M.TH2D("Energy", "Energy", 100, 0, 600, 100, 0, 600)
     #Hist.SetXTitle("Input energy [keV]")
     #Hist.SetYTitle("Measured energy [keV]")
@@ -100,7 +100,8 @@ def load_data(fn, event_hits, maxevents, minhits=1, detectortype=None, maxclass=
 
         NEvents += 1
 
-        if Type not in event_types:
+        if Type not in event_hits:
+            print("Adding:", Type)
             event_hits[Type] = []
 
         event_hits[Type].append(Hits)
@@ -128,23 +129,9 @@ def load_data(fn, event_hits, maxevents, minhits=1, detectortype=None, maxclass=
 
     print("Max Label:", max_label)
 
-    '''
-    shuffledTypes = EventTypes.copy()
-    shuffledHits = EventHits.copy()
-
-    random.shuffle(shuffledHits)
-    random.shuffle(shuffledTypes)
-
-    ceil = math.ceil(len(self.EventHits)*0.75)
-    EventTypesTrain = shuffledTypes[:ceil]
-    EventTypesTest = shuffledTypes[ceil:]
-    EventHitsTrain = shuffledHits[:ceil]
-    EventHitsTest = shuffledHits[ceil:]
-    '''
-
     return event_hits
 
-def gen_dataset(event_hits, nevents):
+def gen_dataset(event_hits, nevents, split):
     """This justs takes a random subset of each event type so that we have
     similar number of events of each type. It returns a list of the hits and types
     for the dataset."""
@@ -164,33 +151,48 @@ def gen_dataset(event_hits, nevents):
             random.shuffle(events)
             events = events[:nevents]
         types = ut*np.ones(len(events), dtype=int)
-        # events_dataset.append(events)
-        # types_dataset.append(types)
 
         dataset_hits = dataset_hits + events
         dataset_types = dataset_types + list(types)
     
+        # zipped = list(zip(dataset_hits, dataset_types))
+    
+        # random.shuffle(zipped)
+        # shuffled_hits, shuffled_types = zip(*zipped)
+    
+        # ceil = np.ceil(len(event_hits)*split).astype(int)
+        # ceil = int(len(event_hits)*split)
+        # event_types_train = shuffled_types[:ceil]
+        # event_types_test = shuffled_types[ceil:]
+        # event_hits_train = shuffled_hits[:ceil]
+        # event_hits_test = shuffled_hits[ceil:]
+    
     return dataset_hits, dataset_types
+    # return (event_hits_train, event_types_train), (event_hits_test, event_types_test)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Preprocess Cosima Sims')
 
-    parser.add_argument('-fn', dest='fn', action='store', help='Filename to parse')
+    # parser.add_argument('-fn', dest='fn', action='store', help='Filename to parse')
     parser.add_argument('-path', dest='path', action='store', help='Path to files')
     parser.add_argument('-nevents', dest='nevents', type=int, action='store', help='Number of events')
     parser.add_argument('-minhits', dest='minhits', type=int, action='store', default=1,
                          help='Mininum number of hits for good event.')
     parser.add_argument('-outfn', dest='outfn', action='store', help='Path to store output file.')
+    # parser.add_argument('-outpath', dest='outpath', action='store', help='Path to output train/val datasets')
     parser.add_argument('-preprocess_only', dest='preprocess_only', action='store_true')
     parser.add_argument('-nevents_dataset', dest='nevents_dataset', type=int, action='store', default=500000)
     parser.add_argument('-dtype', dest='dtype', type=int, action='store', help='Detector type number')
+    parser.add_argument('-split', dest='split', type=float, action='store', help='Training/Validation split')
 
     args = parser.parse_args()
 
     # 1. Load the Cosima sim data, do some filtering, and separate into different event types
-    pattern_glob = os.path.join(args.path, "*.sim.gz")
+    # pattern_glob = os.path.join(args.path, "*.sim.gz")
+    pattern_glob = args.path + "*.sim.gz"
     fns = glob.glob(pattern_glob)
+    print("fns:", fns)
 
     event_hits = {}
     event_hits[0] = []
@@ -198,7 +200,7 @@ if __name__ == '__main__':
 
     maxclass = args.nevents_dataset
     for fn in fns:
-        print("Processing:", fn)
+        print("Processing:", fn, len(event_hits[0]), len(event_hits[1]))
         load_data(fn, event_hits, args.nevents, minhits=args.minhits,
                   maxclass=args.nevents_dataset, detectortype=args.dtype)
         if (maxclass != None) and (len(event_hits[0]) >= maxclass) and (len(event_hits[1]) >= maxclass):
@@ -211,7 +213,18 @@ if __name__ == '__main__':
         exit()
 
     #2. Get similar number of each event type and combine to form a dataset
-    dataset = gen_dataset(event_hits, args.nevents_dataset)
+    dataset = gen_dataset(event_hits, args.nevents_dataset, args.split)
+
+    # print("Size of dataset:", len(dataset[0]))
+    # if args.outfn is not None:
+        # fn_train = args.outfn + "_train.pkl"
+        # fn_val = args.outfn + "_val.pkl"
+        # with open(fn_train, 'wb') as f:
+            # pickle.dump(dataset_train)
+# 
+        # with open(fn_val, 'wb') as f:
+            # pickle.dump(dataset_val)
+
     print("Size of dataset:", len(dataset[0]))
     if args.outfn is not None:
         with open(args.outfn, 'wb') as f:
